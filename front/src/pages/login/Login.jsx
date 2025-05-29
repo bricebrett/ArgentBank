29b94e2e1a62a87eee6d2812e552f6561d870267
Brice <brettinb@hotmail.fr>
2025-05-29 13:14:32 +0200

    feat: add Login page layout

--- front/src/pages/login/Login.jsx
+++ front/src/pages/login/Login.jsx
@@ -1,8 +1,32 @@
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faCircleUser } from "@fortawesome/free-solid-svg-icons";
+import "./login.css";
+
 const Login = () => {
   return (
-    <>
-      <h1>Login Page</h1>
-    </>
+    <section className="main bg-dark">
+      <div className="sign-in-content">
+        <FontAwesomeIcon icon={faCircleUser} className="fontawesome-icon" />
+        <h1>Sign In</h1>
+        <form action="#">
+          <div className="input-wrapper">
+            <label htmlFor="username">Username</label>
+            <input type="text" id="username" />
+          </div>
+          <div className="input-wrapper">
+            <label htmlFor="password">Password</label>
+            <input type="password" id="password" />
+          </div>
+          <div className="input-remember">
+            <input type="checkbox" id="remember-me" />
+            <label htmlFor="remember-me">Remember me</label>
+          </div>
+        </form>
+        <button type="submit" className="sign-in-button">
+          Sign In
+        </button>
+      </div>
+    </section>
   );
 };
 export default Login;
